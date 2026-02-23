1f99831f19dcfe87ba6a6c65b34685245f649245
harshit <harsh.rastogi2704@gmail.com>
2026-02-23 17:33:51 +0530

    Initial Commit

---
+++ TodayOnlyToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodayOnlyToDo
+//
+//  Created by Harshit Rastogi on 23/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodayOnlyToDo/TodayOnlyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayOnlyToDoApp.swift
+//  TodayOnlyToDo
+//
+//  Created by Harshit Rastogi on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TodayOnlyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
